39297c42f5206d60b4f9e7dd76531d8d470b0f3b
Siddhant Satish Parmar <siddhantsatish.parmar@sjsu.edu>
2022-03-01 18:06:03 -0800

    commit 5
    MySQL connection established

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,25 @@
 var express = require ('express');
-var cors=require('cors');
-const app = express();
-
+var cors = require('cors');
 var mysql = require('mysql');
+const router = express.Router();
 
-var dbdata = require('./config.json');
-app.use(cors({origin:dbdata.frontend,credentials:true}));
+var app = express();
+
+var dbdata = require("./config.json");
+
+app.use(cors({ origin:dbdata.frontend, credentials:true }));
+
 var connection = mysql.createPool({
-    host:dbdata.DB.host,
-    username:dbdata.DB.username,
-    password:dbdata.DB.password,
-    port:dbdata.DB.port,
-    database:dbdata.DB.database
+    host: dbdata.DB.host,
+    user: dbdata.DB.username,
+    password: dbdata.DB.password,
+    port: dbdata.DB.port,
+    database: dbdata.DB.database
 });
-console.log(dbdata.DB);
+
 connection.getConnection((err) => {
     if(err){
-        throw 'Error occured: ' + err;
+        throw new 'Error occured: ' + err;
     }
     console.log("pool created");
 });
